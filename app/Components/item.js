48d034e4e6e1dfa2eac8f646036c8f375628340d
MyasnikovIA <Miacom@mail.ru>
2022-01-16 15:04:47 +0700

    Читска кода

--- app/Components/item.js
+++
@@ -1,8 +0,0 @@
-// https://studentaid.psu.edu/real-scripts/ajax/ext/source/widgets/
-Ext.define('widget.item', {
-     extend: 'Ext.Widget',
-     constructor: function(config) {
-         this.callParent([config]);
-         Ext.getBody().appendChild(this.element);
-     },
- });
